dd5e120d70cc61be11c22320ee86298646c2262b
Carolina Valdrighi <69602254+cvaldrighi@users.noreply.github.com>
2020-10-09 15:54:00 -0300

    btns configs - return
    added return option on camera

--- appglitch.js
+++ appglitch.js
@@ -5,10 +5,12 @@ function hasGetUserMedia() {
 if (hasGetUserMedia()) {
     const btnCam = document.getElementById("btnCam");
     const btnSS = document.getElementById("btnSS");
+    const btnReturn = document.getElementById("btnReturn");
     const displayImgBg = document.getElementById("displayImgBg");
     const displayVideo = document.getElementById("displayVideo");
     const canvas = document.createElement("canvas");
     const constraints = {video: {width:600, height:400}};
+    
 
     btnCam.onclick = function() {
         navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
@@ -26,6 +28,8 @@ if (hasGetUserMedia()) {
     };
 
     btnSS.onclick = displayVideo.onclick = function() {
+        displayVideo.classList.add("hidden");
+        btnReturn.classList.remove("hidden");
         canvas.width = displayVideo.videoWidth;
         canvas.height = displayVideo.videoHeight;
         displayImgBg.classList.remove("hidden");
@@ -33,6 +37,12 @@ if (hasGetUserMedia()) {
         displayImgBg.src = canvas.toDataURL("image/webp");
     };
 
+    btnReturn.onclick = function() {
+        displayVideo.classList.remove("hidden");
+        displayImgBg.classList.add("hidden");
+        btnReturn.classList.add("hidden");
+
+    }
 } else {
     alert("Sorry, your browser don't allow this function.");
 }
